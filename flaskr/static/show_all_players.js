ecebda8d43db51049f280bf289bf21171839a9eb
anatolegosset <anatole.gosset@wanadoo.fr>
2023-12-09 22:07:40 +0100

    feat: 34 implemented sort by column for /admin/inscrits

--- flaskr/static/show_all_players.js
+++ flaskr/static/show_all_players.js
@@ -15,7 +15,7 @@ const FrToEn = {
     'N° licence':'licenceNo',
     'Prénom':'firstName',
     'Nom de Famille':'lastName',
-    'N° de Dossard': 'bibNo',
+    'N° dossard': 'bibNo',
     'Club':'club',
     'Adresse Mail':'email',
     'Genre':'gender',
@@ -25,25 +25,29 @@ const FrToEn = {
     'Tableaux':'registeredEntries'
 }
 
+const numericCols = ["bibNo", "licenceNo", "nbPoints"]
+const dontSort = ["registeredEntries", "phone"]
+
+let playerArray;
 
 function putDataInTable(data, elementId) {
-    const playerArray = data;
     const columns = Object.getOwnPropertyNames(FrToEn);
     const table = document.createElement('table');
+    table.id = "players_table";
     const body = document.createElement('tbody');
     const head = document.createElement('thead');
-    console.log(columns);
 
     columns.forEach(function(colName) {
         if (colName != 'Surplus de paiement') {
             let colCell = document.createElement('th');
+            colCell.setAttribute("onclick", "sortByColumn('" + FrToEn[colName] + "')");
             colCell.appendChild(document.createTextNode(colName));
             head.appendChild(colCell);
         }
     });
     table.appendChild(head);
 
-    playerArray.forEach(function(playerObject) {
+    data.forEach(function(playerObject) {
         let row = document.createElement('tr');
         columns.forEach(function(colName) {
             if (colName != 'Surplus de paiement') {
@@ -69,10 +73,37 @@ function putDataInTable(data, elementId) {
 };
 
 function goToPlayerPage(licenceNo) {
-    console.log("pouet");
     window.location.href = "/admin/inscrits/" + licenceNo;
 };
 
+
+let currentSort = {"colName": null, "ascending": true};
+
+function sortByColumn(colName) {
+    if (dontSort.includes(colName)) {
+        return null;
+    }
+    currentSort["ascending"] = (colName == currentSort["colName"]) ? (!(currentSort["ascending"])):true;
+    currentSort["colName"] = colName;
+    if (numericCols.includes(colName)) {
+        playerArray.sort(function(a, b){return a[colName] - b[colName]});
+    } else {
+        playerArray.sort(function(a, b){
+            let x = a[colName].toLowerCase();
+            let y = b[colName].toLowerCase();
+            if (x > y) { return 1; }
+            if (x < y) { return -1; }
+            return 0;
+        });
+    }
+    if (!(currentSort["ascending"])) {
+        playerArray.reverse();
+    }
+    let oldTable = document.getElementById("players_table");
+    oldTable.remove();
+    putDataInTable(playerArray, "players_table_div");
+}
+
 fetch("http://localhost:5000/api/all_players")
         .then(response => { return response.json(); })
-            .then(data => { putDataInTable(data['players'], "tableaux_des_inscrits"); });
+            .then(dataJson => { playerArray=dataJson["players"]; putDataInTable(dataJson["players"], "players_table_div"); });
