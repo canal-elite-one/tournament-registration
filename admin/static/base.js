76cd5ab204153ef953a5597f5bb827354019c04e
anatolegosset <anatole.gosset@wanadoo.fr>
2024-01-14 23:28:42 +0100

    fix: minor changes to both base.js

--- admin/static/base.js
+++ admin/static/base.js
@@ -1,10 +1,8 @@
 function showContent() {
     console.log("showContent");
     document.getElementById("content-div").style.display = "block";
-    // document.getElementById("loading-div").style.display = "none";
 }
 
 function hideContent() {
-    document.getElementById("loading-div").style.display = "block";
-    // document.getElementById("content-div").style.display = "none";
+    document.getElementById("content-div").style.display = "none";
 }
