2ae34116169f87319cb5e9d1e7b0f674ca20f4e1
Celine Lay <layceline@gmail.com>
2024-04-06 23:37:52 +0200

    chore: add redirection from / to /public

--- public/__init__.py
+++ public/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 from public.api import public_api_bp
 from public.front import public_bp, not_found_page
@@ -12,6 +12,10 @@ def create_app(debug=None):
     app.register_blueprint(public_bp)
     app.register_error_handler(404, not_found_page)
     app.register_error_handler(APIError, handle_api_error)
+
+    @app.route("/", methods=["GET"])
+    def index_page():
+        return redirect(url_for("public.index_page"))
 
     app.config.update(get_config_from_env())
 
