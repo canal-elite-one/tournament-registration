9cffb2f18a5fefc1d0dc795a61393362639c3c04
Celine Lay <layceline@gmail.com>
2025-04-06 14:13:59 +0200

    fix: mobile menu

--- front/src/components/SidebarLinks.tsx
+++ front/src/components/SidebarLinks.tsx
@@ -19,18 +19,19 @@ function getNavLinks() {
   ];
 }
 
-export function SidebarLinks() {
+export function SidebarLinks({onClickLink}: { onClickLink?: () => void }) {
   const pathname = usePathname();
 
   return (
-      <Group className="gap-1">
-        {getNavLinks().map((item) => {
+      <Group className="gap-4 flex-col md:flex-row justify-center items-center h-full">
+      {getNavLinks().map((item) => {
           const isActive = pathname === item.link;
 
           return (
               <a
                   key={item.label}
                   href={item.link}
+                  onClick={onClickLink}
                   className={`flex items-center space-x-2 px-3 py-2 rounded-md transition
               ${isActive ? "bg-gray-100 text-[#1836a9] font-medium" : "text-gray-700"}
               hover:bg-gray-100
