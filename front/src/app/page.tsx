d3f8eb22dbf14d409647a442cc7622d1492f6c0c
Celine Lay <layceline@gmail.com>
2025-04-06 20:55:55 +0200

    chore: retrieve player and categories

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import {useEffect, useState} from 'react';
+import {TextInput, Button} from '@mantine/core';
+import {useRouter} from "next/navigation";
 
 export default function HomePage() {
   const [timeLeft, setTimeLeft] = useState<{
@@ -9,9 +11,13 @@ export default function HomePage() {
     minutes: number;
     seconds: number;
   }>({days: 0, hours: 0, minutes: 0, seconds: 0});
+  const [licenceNumber, setLicenceNumber] = useState('');
+  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
-    const targetDate = new Date('2025-05-01T10:00:00');
+    const targetDate = new Date('2025-04-01T10:00:00');
 
     const updateCountdown = () => {
       const now = new Date();
@@ -26,6 +32,7 @@ export default function HomePage() {
         setTimeLeft({days, hours, minutes, seconds});
       } else {
         setTimeLeft({days: 0, hours: 0, minutes: 0, seconds: 0});
+        setIsRegistrationOpen(true);
       }
     };
 
@@ -34,37 +41,58 @@ export default function HomePage() {
     return () => clearInterval(interval);
   }, []);
 
+  const handleLicenceSubmit = async () => {
+    router.push(`/joueur/${licenceNumber}`);
+  };
+
   return (
       <main
-          className="h-screen bg-contain bg-top bg-no-repeat flex justify-center items-center"
-          style={{backgroundImage: "url('/static/countdown-background.jpg')"}}
+          className={`h-screen flex justify-center items-center ${
+              !isRegistrationOpen ? 'bg-contain bg-top bg-no-repeat' : ''
+          }`}
+          style={
+            !isRegistrationOpen
+                ? {backgroundImage: "url('/static/countdown-background.jpg')"}
+                : undefined
+          }
       >
-        <div
-            className="
-          text-center
-          bg-white/30
-          backdrop-blur
-          rounded-xl
-          p-5
-        "
-        >
-          {/* Days */}
-          <div className="flex justify-center mb-4">
-          <span className="text-[3rem] mx-2 text-[#1836a9]">
-            J - {timeLeft.days}
-          </span>
-          </div>
-
-          {/* Countdown Digits */}
-          <div className="flex justify-center space-x-4">
-            <span
-                className="text-[3rem] text-[#1836a9]">{timeLeft.hours}h</span>
-            <span
-                className="text-[3rem] text-[#1836a9]">{timeLeft.minutes}m</span>
-            <span
-                className="text-[3rem] text-[#1836a9]">{timeLeft.seconds}s</span>
-          </div>
-        </div>
+        {isRegistrationOpen ? (
+            <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleLicenceSubmit();
+                }}
+                className="bg-white/30 backdrop-blur rounded-xl p-5 space-y-4 text-center"
+            >
+              <TextInput
+                  placeholder="Enter your licence number"
+                  value={licenceNumber}
+                  onChange={(event) => setLicenceNumber(event.currentTarget.value)}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                      event.preventDefault();
+                      handleLicenceSubmit();
+                    }
+                  }}
+              />
+              <Button type="submit" fullWidth>
+                Submit
+              </Button>
+            </form>
+        ) : (
+            <div className="text-center bg-white/30 backdrop-blur rounded-xl p-5">
+              <div className="flex justify-center mb-4">
+            <span className="text-[3rem] mx-2 text-[#1836a9]">
+              J - {timeLeft.days}
+            </span>
+              </div>
+              <div className="flex justify-center space-x-4">
+                <span className="text-[3rem] text-[#1836a9]">{timeLeft.hours}h</span>
+                <span className="text-[3rem] text-[#1836a9]">{timeLeft.minutes}m</span>
+                <span className="text-[3rem] text-[#1836a9]">{timeLeft.seconds}s</span>
+              </div>
+            </div>
+        )}
       </main>
   );
 }
