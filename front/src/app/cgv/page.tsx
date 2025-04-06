16c1beb913c24bbaa8b62bf50cb0b20a41d9adde
Celine Lay <layceline@gmail.com>
2025-04-06 14:00:47 +0200

    fix: escape special characters for build (#88)

--- front/src/app/cgv/page.tsx
+++ front/src/app/cgv/page.tsx
@@ -4,10 +4,10 @@ export default function CGV() {
         <h1 className="text-2xl font-bold mb-4">Conditions Générales de Vente (CGV)</h1>
 
         <p>Les présentes Conditions Générales de Vente régissent les inscriptions au
-          tournoi national B de l'US Kremlin-Bicêtre.</p>
+          tournoi national B de l&apos;US Kremlin-Bicêtre.</p>
 
         <h2 className="text-xl font-semibold mt-6 mb-2">Tarifs</h2>
-        <p>Frais d'engagement : 10€ par tableau, maximum 2 tableaux par jour.
+        <p>Frais d&apos;engagement : 10€ par tableau, maximum 2 tableaux par jour.
           Possibilité d’un 3e tableau en cas d’élimination dans au moins 1 tableau.</p>
 
         <h2 className="text-xl font-semibold mt-6 mb-2">Inscriptions</h2>
@@ -16,7 +16,7 @@ export default function CGV() {
                   href="https://tournoiuskb.fr">tournoiuskb.fr</a>.</p>
 
         <h2 className="text-xl font-semibold mt-6 mb-2">Paiement</h2>
-        <p>Le paiement est sécurisé via Stripe. Aucune inscription n'est validée sans
+        <p>Le paiement est sécurisé via Stripe. Aucune inscription n&apos;est validée sans
           paiement.</p>
 
         <h2 className="text-xl font-semibold mt-6 mb-2">Remboursements</h2>
