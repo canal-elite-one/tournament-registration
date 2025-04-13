a308c35ff5d06e475b6d2cd63b78b56838503c16
anatole <anatole.gosset@eficia.com>
2025-04-13 17:20:29 +0200

    chore: seeds

--- apis/tests/sample_data/categories.sql
+++ apis/tests/sample_data/categories.sql
@@ -1,7 +1,7 @@
 INSERT INTO public.categories (category_id, alternate_name, color, min_points, max_points, start_time, women_only, base_registration_fee, late_registration_fee, reward_first, reward_second, reward_semi, reward_quarter, max_players, overbooking_percentage) VALUES
 	('A', '< 900', 'blue', 0, 900, '2024-01-06 09:00:00', false, 7, 1, 70, 35, 20, NULL, 72, 20),
 	('B', '< 1500', 'blue', 0, 1500, '2024-01-06 10:15:00', false, 7, 1, 140, 70, 35, NULL, 72, 15),
-	('C', NULL, '#00FF00', 1300, 4000, '2024-01-06 11:30:00', true, 10, 2, 300, 150, 75, NULL, 36, 10),
+	('C', 'Obligatoire F', '#00FF00', 1300, 4000, '2024-01-06 11:30:00', true, 10, 2, 300, 150, 75, NULL, 36, 10),
 	('D', NULL, '#00FF00', 0, 1100, '2024-01-06 12:45:00', false, 7, 1, 100, 50, 25, NULL, 72, 20),
 	('E', NULL, '#FF0000', 1500, 4000, '2024-01-06 14:00:00', false, 10, 2, 300, 150, 75, NULL, 72, 0),
 	('F', 'Pas open féminin', '#FF0000', 0, 1300, '2024-01-06 15:00:00', false, 7, 1, 120, 60, 30, NULL, 72, 10),
@@ -10,6 +10,6 @@ INSERT INTO public.categories (category_id, alternate_name, color, min_points, m
 	('2', NULL, '#0000FF', 0, 1400, '2024-01-07 10:15:00', false, 7, 1, 130, 65, 35, NULL, 72, 0),
 	('3', NULL, '#FFFF00', 0, 2100, '2024-01-07 11:30:00', false, 7, 1, 200, 100, 50, NULL, 72, 30),
 	('4', NULL, '#FFFF00', 0, 1000, '2024-01-07 12:45:00', false, 7, 1, 90, 45, 25, NULL, 72, 20),
-	('5', NULL, NULL, 0, 1600, '2024-01-07 14:00:00', true, 7, 1, 150, 75, 40, NULL, 36, 20),
+	('5', 'Obligatoire F', NULL, 0, 1600, '2024-01-07 14:00:00', true, 7, 1, 150, 75, 40, NULL, 36, 20),
 	('6', NULL, '#00FFFF', 0, 1200, '2024-01-07 15:00:00', false, 7, 1, 110, 55, 30, NULL, 72, 15),
 	('7', NULL, '#00FFFF', 0, 800, '2024-01-07 16:00:00', false, 7, 1, 60, 30, 15, NULL, 72, 10);
