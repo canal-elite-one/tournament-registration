8364a4ef354b5649e17b0c444649e2d874321e85
Celine Lay <layceline@gmail.com>
2025-04-27 19:07:59 +0200

    feat: do not display entries not paid for

--- front/src/app/joueur/[licenceNo]/inscription/page.tsx
+++ front/src/app/joueur/[licenceNo]/inscription/page.tsx
@@ -12,22 +12,27 @@ export default async function Page({params}: {
     const maxOverbooked =  Math.floor(entry.maxPlayers * (1 + entry.overbookingPercentage/100.0));
     const rank = entry.rank;
     const waitingListPosition = rank - maxOverbooked;
+
+    const isInWaitingList = rank > maxOverbooked;
+
+
     let entryString = "Tableau " + entry.categoryId;
     if (entry.alternateName !== null) {
       entryString += " (" + entry.alternateName + ")";
     }
-    if (rank > maxOverbooked) {
+    if (isInWaitingList) {
       entryString += " - " + waitingListPosition + "e sur liste d'attente";
     }
-
     const startTime = new Date(entry.startTime);
     entryString += ", le " + startTime.toLocaleDateString('fr-FR', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
     entryString += " à " + startTime.toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit'});
     return {
       id: entry.categoryId,
       label: entryString,
+      isInWaitingList: isInWaitingList,
+      hasPaid: entry.markedAsPaid
     };
   });
 
-  return <EntriesSummary licenceNo={licenceNo} entries={formattedEntries} emailContact={process.env.USKB_CONTACT_EMAIL}></EntriesSummary>
+  return <EntriesSummary licenceNo={licenceNo} entries={formattedEntries.filter(e => e.hasPaid || e.isInWaitingList)} emailContact={process.env.USKB_CONTACT_EMAIL}></EntriesSummary>
 };
